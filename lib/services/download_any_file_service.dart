837f29453305e19147aa92e5c804d107d22b1c57
Pratik Amonkar <amonkar3000@gmail.com>
2025-02-28 14:40:54 +0530

    download  any file service added

--- lib/services/download_any_file_service.dart
+++ lib/services/download_any_file_service.dart
@@ -1,103 +1,86 @@
-import 'dart:io';
+import 'dart:async';
 
-import 'package:crypto/crypto.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
-import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:permission_handler/permission_handler.dart';
 
-// TODO:: add permission_handler,path_provider,path,flutter_downloader and crypto dependency in pubspec.yaml file
+//Todo: Add provider file in res/xml/flutter_downloader_provider_paths.xml
 
-class DownloadAnyFileService {
-  void initializeDownloader() async {
-    await FlutterDownloader.initialize(debug: true, ignoreSsl: false);
-  }
-
-  void downloadFile() async {
-    try {
-      Directory appDocDir = await getApplicationDocumentsDirectory();
-      String appDocPath = appDocDir.path;
-      String downloadDir = "$appDocPath/test";
-
-      Directory(downloadDir).createSync(recursive: true);
-
-      final taskId = await FlutterDownloader.enqueue(
-          url:
-              'https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg',
-          savedDir: downloadDir,
-          fileName: 'demo1.jpg',
-          showNotification: true,
-          openFileFromNotification: true,
-          saveInPublicStorage: true);
-      debugPrint("taskId = $taskId");
-    } on Exception catch (e) {
-      debugPrint("Catch e = $e");
-    }
-  }
-
-  Future<bool> checkAndRequestStoragePermission() async {
-    var status = await Permission.storage.status;
-    if (!status.isGranted) {
-      status = await Permission.storage.request();
-    }
-    return status.isGranted;
-  }
-
-  Future<String> getFilePath(String filename) async {
-    Directory appDocDir = await getApplicationDocumentsDirectory();
-    String appDocPath = appDocDir.path;
-    return join(appDocPath, filename);
-  }
-
-  void initDownloader() async {
-    final taskId = await FlutterDownloader.enqueue(
-      url: 'https://example.com/file.zip',
-      savedDir: 'path_to_directory',
-      fileName: 'desired_filename.zip',
-      showNotification: true,
-      openFileFromNotification: true,
+class FileDownloadService {
+  static Future<void> initialize() async {
+    await FlutterDownloader.initialize(
+      debug: false,
     );
   }
 
-  Future<void> saveFileLocally(String filename, List<int> bytes) async {
-    Directory directory = await getApplicationDocumentsDirectory();
-    String path = join(directory.path, filename);
-    File file = File(path);
-    await file.writeAsBytes(bytes);
-    debugPrint("File saved at $path");
-  }
-
-  Future<void> attemptSaveFile(String filename, List<int> bytes) async {
-    bool hasPermission = await checkAndRequestStoragePermission();
-    if (hasPermission) {
-      try {
-        await saveFileLocally(filename, bytes);
-        debugPrint("File download and save completed.");
-      } catch (e) {
-        debugPrint("An error occurred while saving the file: $e");
-      }
-    } else {
-      debugPrint("Storage permission not granted. Cannot save the file.");
+  static Future<String?> downloadFile({
+    required String url,
+    required String fileName,
+    required String savePath,
+    bool isImage = false,
+  }) async {
+    final taskId = await FlutterDownloader.enqueue(
+      url: url,
+      savedDir: savePath,
+      fileName: fileName,
+      showNotification: true,
+      openFileFromNotification: true,
+    );
+    if (isImage) {
+      Timer.periodic(
+        const Duration(seconds: 10),
+        (timer) async {
+          if (taskId != null) {
+            bool completed = await checkDownloadStatus(
+                taskId: taskId, path: "$savePath/$fileName");
+            if (completed) {
+              timer.cancel();
+            }
+          }
+        },
+      );
     }
+    return taskId;
   }
 
-  Future<bool> verifyDownloadedFile(
-      String filePath, String originalChecksum) async {
-    File file = File(filePath);
-    List<int> fileBytes = await file.readAsBytes();
-    String fileChecksum = md5.convert(fileBytes).toString();
-
-    return fileChecksum == originalChecksum;
+  static Future<bool> checkDownloadStatus(
+      {required String taskId, required String path}) async {
+    const channel =
+        MethodChannel("com.pratik.flutter_core_concept/media_scanner");
+    final task = await FlutterDownloader.loadTasksWithRawQuery(
+        query: 'SELECT * FROM task WHERE task_id="$taskId"');
+    if (task?.isNotEmpty == true) {
+      final status = task?.first.status;
+      if (status == DownloadTaskStatus.complete) {
+        await channel.invokeMethod(
+          'refreshGallery',
+          {"path": path},
+        );
+        return true;
+      }
+      /*else if (status == DownloadTaskStatus.failed) {
+        debugPrint("Download failed");
+      }
+      else if (status == DownloadTaskStatus.canceled) {
+        debugPrint("Download canceled");
+      }*/
+    }
+    return false;
   }
 
-  void updateDownloadProgress(double progress, BuildContext context) {}
+  static Future<void> cancelDownload(String taskId) async {
+    await FlutterDownloader.cancel(taskId: taskId);
+  }
 
-  void showDownloadCompleteNotification(String filename, BuildContext context) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text("$filename downloaded successfully."),
-      ),
+  static Future<void> deleteDownloadedFile(String taskId) async {
+    await FlutterDownloader.remove(taskId: taskId, shouldDeleteContent: true);
+  }
+
+  static void listenToDownloadProgress(
+      Function(DownloadTaskStatus status, int progress) callback) {
+    FlutterDownloader.registerCallback(
+      (id, status, progress) {
+        callback(DownloadTaskStatus.fromInt(status), progress);
+      },
     );
   }
 }
